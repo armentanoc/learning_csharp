8e795bad5e00e012fdf7a9decbdd89630e798aa7
kérous <88147887+armentanoc@users.noreply.github.com>
2023-10-25 21:03:40 -0300

    Adds explanation.

--- Fundamentals/LongestSubstring.cs
+++ Fundamentals/LongestSubstring.cs
@@ -33,5 +33,31 @@ namespace Fundamentals
 Programming Problem: Longest Substring Without Repeating Characters
 
 Given a string, find the length of the longest substring without repeating characters.
- 
+
+
+Solução:
+
+- Abordagem de slide window para encontrar a subsequência mais longa sem
+caracteres repetidos
+
+- Há dois ponteiros, left e right, que definem a substring atual
+
+- O dicionário charIndexMap é usado para armazenar o índice da última
+ocorrência de cada caractere na string
+
+- Conforme iteramos pela string da esquerda para a direita, se encontrarmos
+um caractere repetido (ou seja, um que já esteja na substring entre left e
+right), atualizamos o valor de left para o próximo índice do caractere
+repetido, garantindo assim que tenhamos uma nova substring sem caracteres
+repetidos
+
+- Atualizamos o dicionário charIndexMap e calculamos o comprimento da
+substring atual, mantendo um registro do comprimento máximo encontrado
+até o momento
+
+- Complexidade de tempo de O(n), porque faz uma única passagem pela
+string de entrada, e as operações do dicionário (inserção e busca) têm
+uma complexidade média de O(1), encontrando eficientemente o comprimento
+da substring mais longa sem caracteres repetidos
+
  */
